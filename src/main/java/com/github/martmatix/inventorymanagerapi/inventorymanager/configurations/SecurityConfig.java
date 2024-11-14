681ff29f517085a30d74031e11109c088217c76d
MartmatiX <martin.malir@gmail.com>
2024-11-14 17:47:15 +0100

    Grants added for endpoints

---
+++ src/main/java/com/github/martmatix/inventorymanagerapi/inventorymanager/configurations/SecurityConfig.java
@@ -0,0 +1,53 @@
+package com.github.martmatix.inventorymanagerapi.inventorymanager.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtDecoder jwtDecoder) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/pokemon/inventory/saveGamba").hasAuthority("svc::inventory_api::route::/pokemon/saveGamba")
+                        .requestMatchers("/pokemon/inventory/changeOwner").hasAuthority("svc::inventory_api::route::/pokemon/changeOwner")
+                        .requestMatchers("/pokemon/inventory/getInventory").hasAuthority("svc::inventory_api::route::/pokemon/getInventory")
+                )
+                .csrf(AbstractHttpConfigurer::disable)
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> {
+                    jwt.decoder(jwtDecoder);
+                    jwt.jwtAuthenticationConverter(jwtAuthenticationConverter());
+                }));
+
+        return http.build();
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            List<String> roles = jwt.getClaimAsStringList("roles");
+            if (roles == null) {
+                return List.of();
+            }
+
+            return roles.stream()
+                    .map(SimpleGrantedAuthority::new)
+                    .collect(Collectors.toList());
+        });
+        return converter;
+    }
+
+}
